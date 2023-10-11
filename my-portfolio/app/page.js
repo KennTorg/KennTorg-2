459304c9d26ac7cc3edeb647bdfcf9efa37d9710
Kenn Torg <kennettorgersenmail@gmail.com>
2023-10-11 23:08:12 +0200

    homepage

--- my-portfolio/app/page.js
+++ my-portfolio/app/page.js
@@ -1,11 +1,23 @@
+import React from "react";
+
 import Header from "./components/header";
-import Welcome from "./components/header/welcome";
+import Welcome from "./components/welcome";
+import About from "./components/about";
+import Work from "./components/work";
+import ContactForm from "./components/contactForm";
+import Footer from "./components/footer";
+
+import styles from "./styles/home.module.scss";
 
 export default function Home() {
   return (
-    <>
+    <div className={styles.home_container}>
       <Header />
       <Welcome />
-    </>
+      <About />
+      <Work />
+      <ContactForm />
+      <Footer />
+    </div>
   );
 }
