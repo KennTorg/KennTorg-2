af3d3fa3badd995e7374fadd384583a1170d5d14
Kenn Torg <kennettorgersenmail@gmail.com>
2023-10-11 23:08:40 +0200

    footer

---
+++ my-portfolio/app/components/footer/index.js
@@ -0,0 +1,43 @@
+"use client";
+
+import React from "react";
+import styles from "./Footer.module.scss";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faFacebookSquare,
+  faInstagram,
+  faTwitterSquare,
+  faGithub,
+  faLinkedin,
+} from "@fortawesome/free-brands-svg-icons";
+
+const Footer = () => {
+  return (
+    <footer className={styles.footer}>
+      <div className={styles.copyright}>
+        <span className={styles.copy}>
+          Copyright &copy; KennTorg | Portfolio2 | 2023
+        </span>
+      </div>
+      <div className={styles.icons}>
+        <a href='https://www.facebook.com'>
+          <FontAwesomeIcon icon={faFacebookSquare} className={styles.icon} />
+        </a>
+        <a href='https://www.instagram.com'>
+          <FontAwesomeIcon icon={faInstagram} className={styles.icon} />
+        </a>
+        <a href='https://twitter.com/home'>
+          <FontAwesomeIcon icon={faTwitterSquare} className={styles.icon} />
+        </a>
+        <a href='https://github.com/KennTorg'>
+          <FontAwesomeIcon icon={faGithub} className={styles.icon} />
+        </a>
+        <a href='https://www.linkedin.com/'>
+          <FontAwesomeIcon icon={faLinkedin} className={styles.icon} />
+        </a>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
